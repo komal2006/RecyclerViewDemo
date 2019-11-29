56ecc939001f881c8a6e98b7a0c8ae749c54f59e
Komal2006 <komaldeepkr99@gmail.com>
2019-11-29 12:52:00 -0500

    finally error removed

--- app/src/main/java/com/example/recyclerviewdemoadapter/Country.java
+++ app/src/main/java/com/example/recyclerviewdemoadapter/Country.java
@@ -6,24 +6,29 @@ public class Country
     private String title;
     private int flag;
 
-    public Country(String title, int flag) {
+    public Country(String title, int flag)
+    {
         this.title = title;
         this.flag = flag;
     }
 
-    public String getTitle() {
+    public String getTitle()
+    {
         return title;
     }
 
-    public void setTitle(String title) {
+    public void setTitle(String title)
+    {
         this.title = title;
     }
 
-    public int getFlag() {
+    public int getFlag()
+    {
         return flag;
     }
 
-    public void setFlag(int flag) {
+    public void setFlag(int flag)
+    {
         this.flag = flag;
     }
 }
